99cce086a5ecb344154660c7fd973896b600d324
Jakub Janarek <jjanarek@gmail.com>
2018-01-14 17:43:41 +0100

    verson 1!! yay!!

--- prai/__version__.py
+++ prai/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'prai'
 __description__ = 'Raiblocks Python RPC client'
 __url__ = "https://github.com/jxub/prai.git"
-__version__ = '0.0.3'
+__version__ = '1.0.0'
 __author__ = 'Jakub Janarek'
 __author_email__ = 'jjanarek@gmail.com'
 __license__ = 'MIT License'
